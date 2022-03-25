f7593b9a105c0dfd83953c767d1f22499188eb84
kks <drather@naver.com>
2022-03-25 18:23:07 +0900

    ch02 단일 체크박스 2: 주석 추가, ItemType 클래스

--- ch02-form/src/main/java/hello/itemservice/domain/item/ItemType.java
+++ ch02-form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -1,5 +1,8 @@
 package hello.itemservice.domain.item;
 
+/**
+ * ItemType 을 나타내는 enum 크래스
+ */
 public enum ItemType {
     BOOK("도서"), FOOD("음식"), ETC("기타");
     private final String description;
