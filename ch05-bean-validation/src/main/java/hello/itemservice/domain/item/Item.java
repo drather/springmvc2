5c5f4a1a4671c86230f46e058bf4258741ab1a9a
kks <drather@naver.com>
2022-04-04 23:58:44 +0900

    ch05 Bean Validation 오브젝트 에러: ScriptAssert 활용한 애노테이션 추가 및 주석

--- ch05-bean-validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ ch05-bean-validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,14 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.quantity * this.price >= 10000", message = "10000이 넘게 해주세요")
 public class Item {
 
     private Long id;
