af72f85bf52509236c5c816e7cd46c73285c8601
kks <drather@naver.com>
2022-04-06 23:41:23 +0900

    ch05 Bean Validation form 분리: 도메인 객체 item 검증 제거

--- ch05-bean-validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ ch05-bean-validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,19 +12,19 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.quantity * this.price >= 10000", message = "10000이 넘게 해주세요")
 public class Item {
 
-    @NotNull(groups = {UpdateCheck.class})
+//    @NotNull(groups = {UpdateCheck.class})
     private Long id;
 
 
-    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백이면 안되어요")
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class}, message = "공백이면 안되어요")
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotNull
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(value = 9999, groups = {SaveCheck.class})
+//    @NotNull
+//    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
